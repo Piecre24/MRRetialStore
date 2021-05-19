792fda6bc81bc31848dcb49ff8c31391dde34f13
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2021-05-20 10:51:20 +1200

    create a cartlist

---
+++ client/components/CartList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const cartlist = (props) => {
+  return (
+    <div className='cart-list'>
+      {/* {props.products.map(product => {
+        return (
+          hi
+        )
+      })} */}
+    </div>
+  )
+}
+export default cartlist
