3ccb0c9bf1ff89e1d4f2c566d17de4ef00bfca0f
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2021-05-19 23:22:50 +1200

    added header react compenent a debug npm run dev script

---
+++ client/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class header extends React.Component {
+  render () {
+    return (
+      <>
+
+        This is header
+
+      </>
+    )
+  }
+}
+
+export default header
